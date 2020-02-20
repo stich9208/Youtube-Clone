1bad59c8399d775798cd6f1adf358d352eeadbe2
stich9208 <54920372+stich9208@users.noreply.github.com>
2020-02-20 23:57:52 +0900

    youtube-clone day3

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-function handleHome(req, res) {
-  res.send("Hello from home!!:)");
-}
 
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
