38bbcb7f78d7171852505d58e387af918b2cfe6a
stich9208 <54920372+stich9208@users.noreply.github.com>
2020-02-24 23:23:10 +0900

    youtube-clone day4

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Youtube Clone
+# WeTube
+
+
+Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,31 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
-export const upload = (req, res) =>
+import { videos } from "../db";
+import routes from "../routes";
+
+export const home = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
+};
+
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, desctiption }
+  } = req;
+  //To Do: Upload and save video
+  res.redirect(routes.videoDetail());
+};
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
