e3d09f80d18bdfd12331fb83e5ea9fe5d03dc656
stich9208 <54920372+stich9208@users.noreply.github.com>
2020-03-19 21:35:14 +0900

    youtube-clone day8

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,23 +2,52 @@ const recorderContainer = document.getElementById("jsRecorderContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById;
 
-const startRecording = async () => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click;
+};
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
-      video: true
+      video: { width: 1200, height: 720 }
     });
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording(stream);
   } catch (error) {
     recordBtn.innerHTML = "😭Can`t record";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
